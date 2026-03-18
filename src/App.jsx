f8f2a7d738c65b27765e9398077ced4ef16b44ba
Ruthwik <ruthwikvarmagudur@gmail.com>
2026-03-18 20:33:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ //components
+// 1.class based component
+//import React from 'react'
+//class App extends React.Component{
+  //  render(){
+    //    return <h1>im ok</h1>
+   // }
+//}
+//export default App
+
+ //2. function based component 
+
+const App=()=> {
+    return <h1>hi hello</h1>
+
+}
+export default App
